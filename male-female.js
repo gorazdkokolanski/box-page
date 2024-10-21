c646a5de4fb7c362424933bd9d5904a37ab0c972
gorazdkokolanski <130471310+gorazdkokolanski@users.noreply.github.com>
2024-10-21 22:59:29 +0200

    Add files via upload

--- male-female.js
+++ male-female.js
@@ -1,8 +1,24 @@
 const video = document.querySelector('video');
+const clickableLink = document.querySelector('.clickable');
 
-console.log(video)
+console.log(video);
 
+let clicked = false
+
+// Pause the video when the link is clicked
+clickableLink.addEventListener('click', (e) => {
+    console.log('Anchor tag clicked!');
+    if(!clicked){
+        video.pause(); // Pause the video
+        clicked = true
+    }
+    else{
+        e.preventDefault()
+        video.play(); // Pause the video
+    }
+});
+
+// Optional: Log when the video ends (if needed)
 video.addEventListener('ended', () => {
-    console.log(video)
-    window.open("https://www.test.de/", "_blank");
+    console.log('Video ended:', video);
 });
